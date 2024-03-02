bfd0adcf0f6f4588962fdcbf33baab50b03a54d4
nyomiamunson <nyomigirl12@gmail.com>
2024-03-02 15:05:04 -0500

    Inputted audio

--- Assets/Explosion/Destroyer.cs
+++ Assets/Explosion/Destroyer.cs
@@ -1,11 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class Destroyer : MonoBehaviour
 {
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>(); //Get a reference to our AudioSource
+    }
+
     void DestroyGameObject()
     {
+        AudioSource audioSource; //Delcare a AudioSource reference variable
+        audioSource = GetComponent<AudioSource>(); //Get a reference to our AudioSource
+        audioSource.Play();
         Destroy(gameObject);
     }
 }
